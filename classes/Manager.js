83070d67ccdb174c4d99cdf21ee6035e9947e454
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-09-08 13:16:49 -0400

    added to Manager.js

--- classes/Manager.js
+++ classes/Manager.js
@@ -6,6 +6,14 @@ class Manager extends Employee {
         super(name, id, email, "manager")
         this.officeNumber = officeNumber;
     }
+
+    getRole() {
+        return "Manager";
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber
+    }
 }
 
 // const test = new Manager("Billy", "110121", "gmail@gmail.com", "05")
